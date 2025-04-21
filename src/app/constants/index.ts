aef16727386e2902f658d4a65435803cc5aa1fa2
Luke Evangelidis <luke.evangelidis@gmail.com>
2025-04-21 11:02:13 +0100

    requirements

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,1 +1,7 @@
-export const URLs = ["https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml", "https://feeds.bbci.co.uk/news/world/europe/rss.xml", "https://www.theguardian.com/uk/rss", "https://www.smh.com.au/rss/feed.xml"]
+export const URLs = [
+  "https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml",
+  "https://feeds.bbci.co.uk/news/world/europe/rss.xml",
+  "https://www.theguardian.com/uk/rss",
+  "https://www.smh.com.au/rss/feed.xml",
+  "https://feeds.washingtonpost.com/rss/world",
+];
